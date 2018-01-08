88cb04da33d48da50c17979aa326e438b1eb6dde
Etienne Curati <etienne.curati@gmail.com>
2018-01-08 17:40:59 -0200

    commit

--- app/assets/javascripts/client.js
+++ app/assets/javascripts/client.js
@@ -1,54 +1,147 @@
-var stream;
-  
-function hasUserMedia() { 
-   //check if the browser supports the WebRTC 
-   return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || 
-      navigator.mozGetUserMedia); 
-} 
- 
-if (hasUserMedia()) {
-   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
-      || navigator.mozGetUserMedia; 
-      
-   //enabling video and audio channels 
-   navigator.getUserMedia({ video: true, audio: true }, function (s) { 
-      stream = s; 
-      var video = document.querySelector('video'); 
-      
-      //inserting our stream to the video tag     
-      video.src = window.URL.createObjectURL(stream); 
-   }, function (err) {}); 
-   
-} else { 
-   alert("WebRTC is not supported"); 
-}
-  
-btnGetAudioTracks.addEventListener("click", function(){ 
-   console.log("getAudioTracks"); 
-   console.log(stream.getAudioTracks()); 
-});
-  
-btnGetTrackById.addEventListener("click", function(){ 
-   console.log("getTrackById"); 
-   console.log(stream.getTrackById(stream.getAudioTracks()[0].id)); 
-});
-  
-btnGetTracks.addEventListener("click", function(){ 
-   console.log("getTracks()"); 
-   console.log(stream.getTracks()); 
-});
- 
-btnGetVideoTracks.addEventListener("click", function(){ 
-   console.log("getVideoTracks()"); 
-   console.log(stream.getVideoTracks()); 
-});
+$(document).ready(function(){
 
-btnRemoveAudioTrack.addEventListener("click", function(){ 
-   console.log("removeAudioTrack()"); 
-   stream.removeTrack(stream.getAudioTracks()[0]); 
-});
-  
-btnRemoveVideoTrack.addEventListener("click", function(){ 
-   console.log("removeVideoTrack()"); 
-   stream.removeTrack(stream.getVideoTracks()[0]); 
+   var connection = new WebSocket('wss://localhost:9090'); 
+   var name = ""; 
+    
+   var loginInput = document.querySelector('#loginInput'); 
+   var loginBtn = document.querySelector('#loginBtn'); 
+   var otherUsernameInput = document.querySelector('#otherUsernameInput'); 
+   var connectToOtherUsernameBtn = document.querySelector('#connectToOtherUsernameBtn'); 
+   var connectedUser, myConnection;
+     
+   //when a user clicks the login button 
+   loginBtn.addEventListener("click", function(event){ 
+      name = loginInput.value; 
+      
+      if(name.length > 0){ 
+         send({ 
+            type: "login", 
+            name: name 
+         }); 
+      } 
+      
+   });
+     
+   //handle messages from the server 
+   connection.onmessage = function (message) { 
+      console.log("Got message", message.data);
+      var data = JSON.parse(message.data); 
+      
+      switch(data.type) { 
+         case "login": 
+            onLogin(data.success); 
+            break; 
+         case "offer": 
+            onOffer(data.offer, data.name); 
+            break; 
+         case "answer": 
+            onAnswer(data.answer); 
+            break; 
+         case "candidate": 
+            onCandidate(data.candidate); 
+            break; 
+         default: 
+            break; 
+      } 
+   };
+     
+   //when a user logs in 
+   function onLogin(success) { 
+
+      if (success === false) { 
+         alert("oops...try a different username"); 
+      } else { 
+         //creating our RTCPeerConnection object 
+         
+         var configuration = { 
+            "iceServers": [{ "url": "stun:stun.1.google.com:19302" }] 
+         }; 
+         
+         myConnection = new webkitRTCPeerConnection(configuration); 
+         console.log("RTCPeerConnection object was created"); 
+         console.log(myConnection); 
+     
+         //setup ice handling
+         //when the browser finds an ice candidate we send it to another peer 
+         myConnection.onicecandidate = function (event) { 
+         
+            if (event.candidate) { 
+               send({ 
+                  type: "candidate", 
+                  candidate: event.candidate 
+               }); 
+            } 
+         }; 
+      } 
+   };
+     
+   connection.onopen = function () { 
+      console.log("Connected"); 
+   };
+     
+   connection.onerror = function (err) { 
+      console.log("Got error", err); 
+   };
+     
+   // Alias for sending messages in JSON format 
+   function send(message) { 
+
+      if (connectedUser) { 
+         message.name = connectedUser; 
+      } 
+      
+      connection.send(JSON.stringify(message)); 
+   };
+
+
+   // setup a peer connection with another user 
+   connectToOtherUsernameBtn.addEventListener("click", function () { 
+    
+      var otherUsername = otherUsernameInput.value; 
+      connectedUser = otherUsername;
+      
+      if (otherUsername.length > 0) { 
+         //make an offer 
+         myConnection.createOffer(function (offer) { 
+            console.log(); 
+            send({ 
+               type: "offer", 
+               offer: offer 
+            });
+            
+            myConnection.setLocalDescription(offer); 
+         }, function (error) { 
+            alert("An error has occurred."); 
+         }); 
+      } 
+   }); 
+    
+   //when somebody wants to call us 
+   function onOffer(offer, name) { 
+      connectedUser = name; 
+      myConnection.setRemoteDescription(new RTCSessionDescription(offer)); 
+      
+      myConnection.createAnswer(function (answer) { 
+         myConnection.setLocalDescription(answer); 
+         
+         send({ 
+            type: "answer", 
+            answer: answer 
+         }); 
+         
+      }, function (error) { 
+         alert("oops...error"); 
+      }); 
+   }
+     
+   //when another user answers to our offer 
+   function onAnswer(answer) { 
+      myConnection.setRemoteDescription(new RTCSessionDescription(answer)); 
+   } 
+    
+   //when we got ice candidate from another user 
+   function onCandidate(candidate) { 
+      myConnection.addIceCandidate(new RTCIceCandidate(candidate)); 
+   }
+
 });
